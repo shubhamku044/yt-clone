b96ffb1fe774abdba24938ba00d39d87831207ea
Shubham Sharma <shubhamku044@gmail.com>
2023-11-07 22:30:19 +0530

    connected to mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-import fs from 'fs';
+import dotenv from 'dotenv';
+import express, { Express } from 'express';
+import connectDB from './db/index.js';
+
+dotenv.config({
+  path: '../.env'
+});
+
+const app: Express = express();
+
+connectDB();
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.info(`app is listening on port ${PORT}...`);
+});
